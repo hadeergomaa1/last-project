1cb9a91353f4ce37c9c0b42c648f71224c6a2e25
hadeergomaa1 <hadeergomaa1717@gmail.com>
2025-09-09 22:02:22 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/contact">Contact</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+nav {
+  margin-bottom: 1rem;
+}
+</style>
